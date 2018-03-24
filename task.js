d28c9a17554baca9feba5f93c87702792c64ba5a
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-03-24 18:35:30 -0400

    Update task.js

--- task.js
+++ task.js
@@ -1,1 +1,59 @@
-//Todo
+const request = require('request');
+const USER_AGENT = process.env.USER_AGENT;
+const WEBHOOK_KEY = process.env.WEBHOOK_KEY;
+const API_KEY = process.env.API_KEY;
+function doNightlyUpdate(){
+  var data = undefined;
+  request(API_KEY, function(error, response, body){
+    data = JSON.parse(body);
+  });
+  if (!data){
+    console.warn("Can not fetch data");
+    return;
+  }
+  var _fields = [
+    {
+      name: "⭐ Favorites",
+      value: data.FavoritedCount,
+      inline: true
+    },
+    {
+      name: "👍 Likes",
+      value: data.TotalUpVotes,
+      inline: true
+    },
+    {
+      name: "👎 Dislikes",
+      value: data.TotalDownVotes,
+      inline: true
+    },
+    {
+      name: "👁️ Plays",
+      value: data.VisitedCount,
+      inline: true
+    },
+    {
+      name: "👨 Online",
+      value: data.OnlineCount,
+      inline: true
+    },
+    {
+      name: "⬆️ Last Updated",
+      value: data.Updated,
+      inline: true
+    }
+  ]
+  var unix = Math.round(+new Date()/1000);
+  request.post('http://service.com/upload', {
+    form: {
+      embeds:{{
+        title: "Nightly Update",
+        color: 3394815,
+        fields: _fields,
+        timestamp: unix
+      }}
+    }
+  })
+}
+doNightlyUpdate();
+setInterval(doNightlyUpdate, 86400000);
