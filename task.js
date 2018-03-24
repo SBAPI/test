e864f9aa45a2440f49a9d6c3882d455df9f0348a
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-03-24 18:41:38 -0400

    Update task.js

--- task.js
+++ task.js
@@ -4,60 +4,54 @@ const WEBHOOK_KEY = process.env.WEBHOOK_KEY;
 const API_KEY = process.env.API_KEY;
 function doNightlyUpdate(){
   var data = undefined;
-  request(API_KEY, function(error, response, body){
-    console.log(body);
-    data = JSON.parse(body);
+  request(API_KEY, function(error, response, data){
+    var _fields = [
+      {
+        name: "⭐ Favorites",
+        value: data.FavoritedCount,
+        inline: true
+      },
+      {
+        name: "👍 Likes",
+        value: data.TotalUpVotes,
+        inline: true
+      },
+      {
+        name: "👎 Dislikes",
+        value: data.TotalDownVotes,
+        inline: true
+      },
+      {
+        name: "👁️ Plays",
+        value: data.VisitedCount,
+        inline: true
+      },
+      {
+        name: "👨 Online",
+        value: data.OnlineCount,
+        inline: true
+      },
+      {
+        name: "⬆️ Last Updated",
+        value: data.Updated,
+        inline: true
+      }
+    ]
+    var unix = Math.round(+new Date()/1000);
+    request.post('http://service.com/upload', {
+      form: {
+        embeds:[{
+          title: "Nightly Update",
+          color: 3394815,
+          fields: _fields,
+          timestamp: unix
+        }]
+      },
+      headers: {
+        "User-Agent": USER_AGENT
+      }
+    })
   });
-  if (data === undefined){
-    console.warn("Can not fetch data");
-    return;
-  }
-  var _fields = [
-    {
-      name: "⭐ Favorites",
-      value: data.FavoritedCount,
-      inline: true
-    },
-    {
-      name: "👍 Likes",
-      value: data.TotalUpVotes,
-      inline: true
-    },
-    {
-      name: "👎 Dislikes",
-      value: data.TotalDownVotes,
-      inline: true
-    },
-    {
-      name: "👁️ Plays",
-      value: data.VisitedCount,
-      inline: true
-    },
-    {
-      name: "👨 Online",
-      value: data.OnlineCount,
-      inline: true
-    },
-    {
-      name: "⬆️ Last Updated",
-      value: data.Updated,
-      inline: true
-    }
-  ]
-  var unix = Math.round(+new Date()/1000);
-  request.post('http://service.com/upload', {
-    form: {
-      embeds:[{
-        title: "Nightly Update",
-        color: 3394815,
-        fields: _fields,
-        timestamp: unix
-      }]
-    },
-    headers: {
-      "User-Agent": USER_AGENT
-    }
-  })
 }
 doNightlyUpdate();
 setInterval(doNightlyUpdate, 86400000);
