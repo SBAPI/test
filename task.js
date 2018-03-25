a1ba46042c25ed93f22f0bbc97872924d6aaf787
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-03-25 16:33:46 -0400

    Update task.js

--- task.js
+++ task.js
@@ -99,7 +99,7 @@ client.on('message', message => {try{
   if (message.content.toLowerCase().substr(0,5) == ">deny"){
     var cmd = message.content
     console.log(cmd)
-    var match = cmd.match(/\<\@\w+\>/)[0];
+    var match = cmd.match(/\<\@\!\w+\>/)[0];
     var userId = match.substr(2, match.length-3);
     var reason = cmd.substr(userId.length+10);
     message.member.guild.members.get("id", userId).send({embed:{
