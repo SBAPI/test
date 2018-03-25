105ac0e2d78936123bc09cca43f3c9c8878b1c22
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-03-24 20:53:20 -0400

    Update task.js

--- task.js
+++ task.js
@@ -54,11 +54,34 @@ client.on('ready', () => {
   ready = 1;
   var stat = 0;
   setInterval(function(){
-	  client.user.setPresence({ game: { name: `Sebby's Script Builder`, type: 1 } });
+	  client.user.setPresence({ game: { name: `>help`, type: 2 } });
   },10000)
   doNightlyUpdate();
   setInterval(doNightlyUpdate, 86400000);
 })
+client.on('message', message => {try{
+  if (message.content.toLowerCase() == ">help"){
+    message.reply(`
+**Help**:
+*>getscore*: __Gets your all time point balance__
+    `);
+  }
+  if (message.content.toLowerCase() == ">getscore"){
+    var match = message.author.username.match(/\(\w+\)/)[0]
+    var user = match.substr(1, match.length-2)
+    if (!user){
+      message.reply("You must be verifyed to use this command. Say `!verify` in <#402320341654962176> to use this command.");
+      return;
+    }
+    var userId = undefined;
+    request("http://api.roblox.com//users/get-by-username?username="+user, function(error, response, body){
+      userId = JSON.parse(body).Id;
+    })
+    request("https://points.roblox.com/v1/universes/556272591/users/"+userId+"/all-time", function(error, response, body){
+      message.reply("Your all time points: `"+JSON.parse(body).allTimeScore+"`");
+    })
+  }
+}catch(err){console.log(err.message)}})
 // Login
 client.login(BOT_KEY);
 setInterval(function(){
